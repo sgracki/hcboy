77964e65425a6b398a1213355c81b86300c196f7
sgracki <szymon@wandlee.com>
2018-11-25 09:45:56 +0100

    s

--- app/app.js
+++ app/app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 1001
 const bodyParser = require('body-parser')
+const path = require('path')
 
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({extended: true,limit: '50mb'}))
 
 app.use('/api/v1/', require('./routes/index.route'));
+app.use('/', express.static(path.join(__dirname, '../web/dist/web')));
+app.use('/**', express.static(path.join(__dirname, '../web/dist/web')));
+app.use('/*', express.static(path.join(__dirname, '../web/dist/web')))
 
 app.listen(port, () => console.log(`Healthcare backend on port ${port}!`))
--- app/controllers/twitter.controller.js
+++ app/controllers/twitter.controller.js
@@ -1,6 +1,6 @@
 
-const TweetStream = require('./utils/lib/tweet-stream');
-const streamToPromise = require('stream-to-promise')
+// const TweetStream = require('./utils/lib/tweet-stream');
+// const streamToPromise = require('stream-to-promise')
 
 function getTweets(req, res, next) {
     const { query } = req.body
