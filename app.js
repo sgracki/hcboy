4f431f12b5465fb5059cb86dfb7f53867111b762
sgracki <szymon@wandlee.com>
2018-11-25 10:43:44 +0100

    e

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({extended: true,limit: '50mb'}))
 
 app.use('/api/v1/', require('./routes/index.route'));
-app.use('/', express.static(path.join(__dirname, './web/dist/web')));
-app.use('/**', express.static(path.join(__dirname, './web/dist/web')));
-app.use('/*', express.static(path.join(__dirname, './web/dist/web')))
+app.use('/', express.static(path.join(__dirname, './web/dist/web')))
 
 app.listen(port, () => console.log(`Healthcare backend on port ${port}!`))
