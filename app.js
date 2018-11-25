e9c4862538a263d7ecd6617cff376fb6db470771
sgracki <szymon@wandlee.com>
2018-11-25 11:20:01 +0100

    heroku build

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 1001
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const path = require('path')
 
@@ -8,6 +8,6 @@ app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({extended: true,limit: '50mb'}))
 
 app.use('/api/v1/', require('./routes/index.route'));
-app.use('/', express.static(path.join(__dirname, './web/dist/web')))
+app.use('/', express.static(path.join(__dirname, './web/dist')))
 
 app.listen(port, () => console.log(`Healthcare backend on port ${port}!`))
