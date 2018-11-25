8e72c04453ade88342ea859fb655f3705a5968f8
Bartlomiej Zając <siubidu@MacBook-Pro-Bartlomiej.local>
2018-11-25 05:55:40 +0100

    JECHANE Z DANYMI MYYYYYYK

---
+++ app/routes/twitter.route.js
@@ -0,0 +1,8 @@
+const twitterCtrl = require('../controllers/twitter.controller')
+const express = require('express')
+const router = express.Router()
+
+router.route('/')
+    .post(twitterCtrl.getTweets)
+
+module.exports = router
